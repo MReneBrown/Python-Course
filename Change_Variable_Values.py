c2241ced5543578ad2d6a84a30e2501cc44d3b23
M Rene Brown <rene_brown1@hotmail.com>
2019-11-04 11:32:33 -0600

    Change_Variable_Values.py

---
+++ Change_Variable_Values.py
@@ -0,0 +1,39 @@
+# meal_completed = True
+# total = 100
+# tip = total * 1/5
+# total = total + tip
+# receipt = "Your total is " +str(total)
+# print(receipt)
+
+first = 'Springer'
+second = 'Bregman'
+third = 'Altuve'
+
+# print(second)
+
+# second = 0.280 
+#  ^^^ Redfinition of second type from str to float
+# the warning states is that we've redefined the 
+# second type from a string into a float. In a 
+# real-world application you could have called a 
+# method on second by making sure the format is 
+# correctly set up with a capital letter in the 
+# first name, etc. Had you done this and accidentally 
+# set it as a float (the batting average), your 
+# program would run into an error because the expected 
+# type is not what has been received. Considered poor practice.
+
+print(second)
+
+
+first = 'Springer'
+second = 'Bregman'
+third = 'Altuve'
+
+print(first)
+
+first = third
+
+print(first)
+
+
