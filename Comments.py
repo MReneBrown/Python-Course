78d0d5323ceefc4e8c9e5f45d1f70b5d4e164da3
M Rene Brown <rene_brown1@hotmail.com>
2019-11-04 12:15:21 -0600

    Comments.py

---
+++ Comments.py
@@ -0,0 +1,20 @@
+# Single Line Comment
+# I'm a single line comment
+
+
+#Inline Comment
+name = 'Kristine Judgens' #TODO: Split into two variables
+print(name)
+#TODO means it needs to be come back to in order to finish it
+
+
+"""
+Multi Line Comment-Multi Line Strings notated by the three 
+quotes before and after
+
+Aenean Lacinia bibendum mulla sed consectetur
+
+Aenean Lacinia bibendum mulla sed consectetur
+
+Aenean Lacinia bibendum mulla sed consectetur
+"""
