970fc40ca083e7336ab807082e73e93e883da96a
M Rene Brown <rene_brown1@hotmail.com>
2019-11-05 13:23:22 -0600

    String_Case_Functions.py

---
+++ String_Case_Functions.py
@@ -0,0 +1,28 @@
+# sentence = 'The quick brown fox jumped'
+# sentence -> variable
+# 'The quick brown fox jumped' -> string
+# = -> assignment of string to variable
+
+
+# sentence = 'The quick brown fox jumped'
+# sentence_two = sentence.upper()
+
+# print(sentence)
+# print(sentence_two)
+
+# The quick brown fox jumped
+# THE QUICK BROWN FOX JUMPED
+
+# sentence = 'the quick brown fox jumped' .capitalize()
+
+# THE QUICK BROWN FOX JUMPED
+
+# sentence = 'the quick brown fox jumped' .title()
+
+# The Quick Brown Fox Jumped
+
+sentence = 'THE QUICK BROWN FOX JUMPED' 
+
+print(sentence.lower())
+
+the quick brown fox jumped
