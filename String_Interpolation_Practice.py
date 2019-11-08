ecfe1d3dcd57a92767895559c6ce9ab637ea4e0e
M Rene Brown <rene_brown1@hotmail.com>
2019-11-08 17:05:19 -0600

    String_Interpolation_Practice.py

---
+++ String_Interpolation_Practice.py
@@ -0,0 +1,20 @@
+name = 'Rene'
+product = 'Full Stack Programming for Dummies'
+team = 'The Dummies Team'
+
+message = f"""
+
+Dear {name},
+
+Thank you for ordering the most amazing Full Stack Programing for Dummies book ever written.
+
+Your order is payment has processed and you will be notified once shipment has taken place.  
+You will be able to track the shipment with the tracking number ZOE834894-23HSN.
+
+Thank you for your order.  Your satisfaction is 100% guaranteed or your money back.
+
+Sincerely,
+
+{team}
+"""
+print(message)
