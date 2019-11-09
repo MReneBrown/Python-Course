59891257ca0d77a03600bc85d350a9c659ebd3d9
M Rene Brown <rene_brown1@hotmail.com>
2019-11-09 14:54:34 -0600

    Neg_Index_String.py

---
+++ Neg_Index_String.py
@@ -0,0 +1,13 @@
+# sentence = 'The quick brown fox jumped over the lazy dog.'
+
+# print(sentence[-1])
+
+# .
+
+
+sentence = 'The quick brown fox jumped over the lazy dog.'
+
+print(sentence[-9:])
+
+lazy dog.
+
