4247cb02ba392824d3d6769001306da23bb45ada
M Rene Brown <rene_brown1@hotmail.com>
2019-11-09 14:31:53 -0600

    Find_Replace_Str_Values.py

---
+++ Find_Replace_Str_Values.py
@@ -0,0 +1,19 @@
+# sentence = 'The quick brown fox jumped over the lazy dog.'
+
+# sentence = 'New Value'
+
+# print(sentence)
+
+# New Value
+
+
+sentence = 'The quick brown fox jumped over the lazy dog.'
+
+sentence = sentence.replace('quick', 'slow')
+
+# print(sentence.replace('quick', 'slow'))
+
+print(sentence)
+
+The slow brown fox jumped over the lazy dog.
+
