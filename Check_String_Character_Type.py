6dc88fb5c682561c3f2c08f4c5de21792d1e4efd
M Rene Brown <rene_brown1@hotmail.com>
2019-11-10 20:32:16 -0600

    Check_String_Character_Type.py

---
+++ Check_String_Character_Type.py
@@ -0,0 +1,20 @@
+# api_data = '5'
+# greeting = 'Hi'
+
+# print(api_data.isalpha())
+# print(greeting.isalpha())
+
+# False
+# True
+
+# print(api_data.isnumeric())
+# print(greeting.isnumeric())
+
+# True
+# False
+
+
+api_data = '5'
+greeting = 'Hi there' # space " " causes a false negative because it isn't alpha/numeric
+
+print(greeting.isalpha())
