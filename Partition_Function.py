18003d7dc68ab9eaf1dce4718077e97951fccdc2
M Rene Brown <rene_brown1@hotmail.com>
2019-11-10 18:57:21 -0600

    Partition_Function.py

---
+++ Partition_Function.py
@@ -0,0 +1,40 @@
+# heading = "Python: An Introduction"
+
+# header, _, subheader = heading.partition(': ')  #heading.partition creates a tuple
+# # the "header", "_", "subheader" are not exact syntax it is just best practice
+
+# print(header)
+# print(_)  # we do not want this because we put that argument into the heading.partition
+# print(subheader)
+
+# Python
+# :
+# An Introduction
+
+#----------------------------------------------------------------------
+
+# heading = "Python: An Introduction"
+
+# first, second, third = heading.partition(': ')  
+
+# print(first)
+# print(second) 
+# print(third)
+
+# Python
+# :
+# An Introduction
+
+# -------------------------------------------------------------------
+
+heading = "Python: An Introduction, and Python: Advanced"
+
+first, second, third = heading.partition(': ')  
+
+print(first)
+print(second) 
+print(third)
+
+Python
+:
+An Introduction, and Python: Advanced
