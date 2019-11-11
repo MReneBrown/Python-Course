675995d9a89f4364139c7c719112eec4e843ff65
M Rene Brown <rene_brown1@hotmail.com>
2019-11-10 20:03:28 -0600

    Split_Function.py

---
+++ Split_Function.py
@@ -0,0 +1,32 @@
+# heading = "Python: An Introduction"
+
+# header, _, subheader = heading.partition(': ')  
+
+# print(header)
+# print(subheader)
+
+
+# tags = 'python,coding,programming,development'
+
+# list_of_tags = tags.split(',') #breaks each one down to its own element
+
+# print(list_of_tags)
+
+# # ['python', 'coding', 'programming', 'development']  each is a stand along string
+
+# list_of_tags = tags.split()   #does not break each one down into its own element
+
+# print(list_of_tags)
+
+# ['python,coding,programming,development']  one string contaning whole collection
+
+
+
+heading = "Python: An Introduction"
+
+heading_list = heading.split(': ')
+
+print(heading_list)
+
+['Python', 'An Introduction']
+
