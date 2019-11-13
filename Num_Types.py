02370eff07bfeaceda7d3f5aaba22e09c740f0e2
M Rene Brown <rene_brown1@hotmail.com>
2019-11-13 14:13:44 -0600

    Num_Types.py

---
+++ Num_Types.py
@@ -0,0 +1,14 @@
+product_id = 123    # integer - whole number
+sale_price = 14.99      # float(not precise)precision is not as granular as decimal precision
+tip_percentage = 1/5    # 20%
+new_product = 150       # integer
+
+print(product_id + new_product)  # 273
+
+# print(sale_price + new_product)  164.99
+
+
+# print(product_id) 123
+# print(sale_price)  14.99
+# print(tip_percentage)  0.2
+# print(new_product)  150
