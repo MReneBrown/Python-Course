4c1e1353071cfa4826ed06b84c8c1f8344ea333a
M Rene Brown <rene_brown1@hotmail.com>
2019-11-13 14:44:29 -0600

    Math_Ops.py

---
+++ Math_Ops.py
@@ -0,0 +1,74 @@
+print('Addition')
+# print(100 + 42)
+addition_value = 100 + 42
+print(addition_value)
+
+
+print('---')
+
+print('Subtraction')
+print(100 - 42.3)
+
+
+print('---')
+
+print('Division')
+print(100 / 42.3)   
+print(100 / 42)
+print(100 / 38)
+
+print('---')
+
+print('Floor Division')  # rounds by dropping all after decimal
+print(100 // 42.3)
+print(100 // 42)
+print(100 // 38)
+
+print('---')
+
+print('Multiplication')
+print(100 * 38)
+
+print('---')
+
+print('Modulus')  # even number will be 0, if odd always returns 1-Returns remainder
+print(100 % 42)
+print(2 % 2)
+print(5 % 2)
+print(25 % 2)
+
+
+print('---')
+
+print('Exponents')  # 25 squared 25 * 25
+print(25 ** 2)
+
+# ------------------------------------------------
+
+# Addition
+# 142
+# ---
+# Subtraction
+# 57.7
+# ---
+# Division
+# 2.364066193853428
+# 2.380952380952381
+# 2.6315789473684212
+# ---
+# Floor Division
+# 2.0
+# 2
+# 2
+# ---
+# Multiplication
+# 3800
+# ---
+# Modulus
+# 16
+# 0
+# 1
+# 1
+# ---
+# Exponents
+# 625
