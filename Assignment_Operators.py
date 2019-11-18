a7a5edf294a174c4a5d55d6986af3c42d02a8b7f
M Rene Brown <rene_brown1@hotmail.com>
2019-11-17 19:36:07 -0600

    Assignment_Operators.py

---
+++ Assignment_Operators.py
@@ -0,0 +1,25 @@
+total = 100
+
+# total = total + 10     110
+# total += 10            110
+
+# total -=10              90
+
+# total *= 2             200
+
+# total /= 10           10.0        
+
+# total //= 10            10     
+
+# total **= 2          10000
+
+# total %= 2             0   #Modulus 11=even number(0)
+
+product_two = 120
+product_three = 10
+
+total += product_two
+total += product_three
+
+
+print(total)
