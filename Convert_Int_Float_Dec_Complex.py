2b16e31d69688f4b69d53e9546001a19d3d35d97
M Rene Brown <rene_brown1@hotmail.com>
2019-11-18 10:23:24 -0600

    Convert_Int_Float_Dec_Complex.py

---
+++ Convert_Int_Float_Dec_Complex.py
@@ -0,0 +1,22 @@
+from decimal import Decimal
+
+product_cost = 88.80     #float
+commission_rate = 0.08
+qty = 450
+
+# print(product_cost * qty)
+# 39780.0
+
+print(int(product_cost))   #float into integer
+88
+
+print(float(qty))    #Integer into float
+450.0
+
+print(Decimal(product_cost))  #converts into decimal
+88.7999999999999971578290569595992565155029296875
+
+print(complex(commission_rate))  #scientific calculation
+(0.08+0j)
+
+
