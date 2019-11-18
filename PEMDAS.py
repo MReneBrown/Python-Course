d811a12fced08a9bbf0a65b7f26e91a2c2c4d98b
M Rene Brown <rene_brown1@hotmail.com>
2019-11-17 19:17:56 -0600

    PEMDAS.py

---
+++ PEMDAS.py
@@ -0,0 +1,23 @@
+"""
+Order of operations
+Please => () Parenthesis
+Excuse => ** Exponents
+My => * Multiplication
+Dear => / Division
+Aunt => + Addition
+Sally => - Subtraction
+
+"""
+
+calculation = 8 + 2 * 5 - (9 + 2) ** 2
+
+print(calculation)
+
+"""
+8 + 2 * 5 - (9 + 2) ** 2
+8 + 2 * 5 - 11 ** 2   (11*11)
+8 + 2 * 5 - 121
+8 + 10 - 121
+18 - 121
+-103
+"""
