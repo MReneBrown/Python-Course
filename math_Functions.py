e4d6833b63d25624d367a0717af5c0b6b846e227
M Rene Brown <rene_brown1@hotmail.com>
2019-11-18 13:57:32 -0600

    Math_Functions.py

---
+++ math_Functions.py
@@ -0,0 +1,20 @@
+import math
+
+loss = -45.40
+product_cost = 99.50
+
+print(abs(loss))
+print(math.floor(product_cost))
+print(math.ceil(loss))
+print(abs(product_cost))
+print(abs(math.floor(loss)))
+print(round(product_cost))
+print(math.sqrt(product_cost))
+
+45.4
+99
+-45
+99.5
+46
+100
+9.974968671630002
