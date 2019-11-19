a0d001a04f97c5457b1b4f0b41660f3359b8d7c8
M Rene Brown <rene_brown1@hotmail.com>
2019-11-18 18:23:58 -0600

    Lambda_Practice.py

---
+++ Lambda_Practice.py
@@ -0,0 +1,23 @@
+# x = lambda a: "This is the "+a
+# print(x("poftut.com"))
+
+# x = lambda a: a*a*a
+# print(x(3))
+# print(x(4))
+
+# This is the poftut.com
+# 27
+# 64
+
+
+x = lambda : "This is the X"
+
+print(x())
+# <function <lambda> at 0x7fbb05b0168>
+
+type(x)
+
+
+This is the X
+
+
