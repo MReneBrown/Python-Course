b3659bef115726c12234ce5abff51d5ace660047
M Rene Brown <rene_brown1@hotmail.com>
2019-11-23 17:22:00 -0600

    List_Sort.py

---
+++ List_Sort.py
@@ -0,0 +1,28 @@
+tags = ['python', 'development', 'tutorials', 'code']
+
+print(tags)
+
+tags.sort()
+
+print(tags)
+
+tags.sort(reverse=True)
+
+print(tags)
+
+
+totals = [234, 1, 543, 2345]
+
+totals.sort()
+
+print(totals)
+
+totals.sort(reverse=True)
+
+print(totals)
+
+['python', 'development', 'tutorials', 'code']
+['code', 'development', 'python', 'tutorials']
+['tutorials', 'python', 'development', 'code']
+[1, 234, 543, 2345]
+[2345, 543, 234, 1]
