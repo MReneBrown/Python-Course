3c9735b347d7d51748bdd7578994f7d47e9ae1d6
M Rene Brown <rene_brown1@hotmail.com>
2019-11-23 14:37:24 -0600

    Lists.py

---
+++ Lists.py
@@ -0,0 +1,99 @@
+# """
+# User Database Query
+# Kristine
+# Tiffany
+# Jordan
+# """
+
+# users = ['Kristine', 'Tiffany', 'Jordan']
+# users2 = ['Kristine' 'Tiffany' 'Jordan']
+# print(users)
+# print(users2)
+
+# users.insert(0, 'Anthony')  
+
+# print(users)
+
+
+# ['Kristine', 'Tiffany', 'Jordan']
+# ['KristineTiffanyJordan']
+# ['Anthony', 'Kristine', 'Tiffany', 'Jordan']
+
+
+
+# users = ['Kristine', 'Tiffany', 'Jordan']
+
+# print(users)
+
+# users.insert(1, 'Anthony')  
+
+# print(users)
+
+# ['Kristine', 'Tiffany', 'Jordan']
+# ['Kristine', 'Anthony', 'Tiffany', 'Jordan']
+
+
+# users = ['Kristine', 'Tiffany', 'Jordan']
+
+# print(users)
+
+# users.insert(1, 'Anthony')  
+
+# print(users)
+
+# users.append('Ian')
+
+# print(users)
+
+# ['Kristine', 'Tiffany', 'Jordan']
+# ['Kristine', 'Anthony', 'Tiffany', 'Jordan']
+# ['Kristine', 'Anthony', 'Tiffany', 'Jordan', 'Ian']
+
+
+
+# users = ['Kristine', 'Tiffany', 'Jordan']
+
+# print(users)
+
+# users.insert(1, 'Anthony')  
+
+# print(users)
+
+# users.append('Ian')
+
+# print(users)
+
+# print(users[2])
+# print([users[2]])
+
+# ['Kristine', 'Tiffany', 'Jordan']  #list
+# ['Kristine', 'Anthony', 'Tiffany', 'Jordan']  #list
+# ['Kristine', 'Anthony', 'Tiffany', 'Jordan', 'Ian']  #list
+# Tiffany   #specific element/object, string Tiffany
+# ['Tiffany']
+
+users = ['Kristine', 'Tiffany', 'Jordan']
+
+print(users)
+
+users.insert(1, 'Anthony')  
+
+print(users)
+
+users.append('Ian')
+
+print(users)
+
+print(users[2])
+print([users[2]])
+
+users[4] = 'Brayden' #not changing Ian just reassigning Brayden
+
+print(users)
+
+['Kristine', 'Tiffany', 'Jordan']
+['Kristine', 'Anthony', 'Tiffany', 'Jordan']
+['Kristine', 'Anthony', 'Tiffany', 'Jordan', 'Ian']
+Tiffany
+['Tiffany']
+['Kristine', 'Anthony', 'Tiffany', 'Jordan', 'Brayden']
