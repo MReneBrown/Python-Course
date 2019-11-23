2c5cb826602103a1354e0be02bc9d549aedd710a
M Rene Brown <rene_brown1@hotmail.com>
2019-11-23 16:06:42 -0600

    4

---
+++ List_Query_Processes.py
@@ -0,0 +1,21 @@
+tags = ['python', 'development', 'tutorials', 'code']
+
+number_of_tags = len(tags)
+
+print(number_of_tags)
+
+# last_item_index = tags.pop()
+# print(last_item_index)
+
+last_item = tags [-1]  
+# -1 goes to the last item first and works backwards from end of list
+index_of_last_item = tags.index(last_item)
+
+
+print(last_item)
+print(index_of_last_item)
+
+
+4
+code
+3
