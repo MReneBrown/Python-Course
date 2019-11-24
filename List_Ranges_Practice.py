a096a3b3c60177de84a9371a599a81b19db7ae16
M Rene Brown <rene_brown1@hotmail.com>
2019-11-24 10:49:38 -0600

    List_Adv_Ranges_Slices.py

--- List_Ranges_Practice.py
+++ List_Ranges_Practice.py
@@ -56,18 +56,4 @@ print(fifth_item)
 [8535481567, 595158, 64891, 2548, 94, 54, 5, 2, 0]
 clarinet
 xylophone
-
-
-
-['saxophone', 'clarinet', 'trumpet', 'trombone', 'french horn', 'tuba']
-['drum', 'flute']
-['guitar']
-10
-['clarinet', 'drum', 'flute', 'french horn', 'guitar', 'saxophone', 'trombone', 'trumpet', 'tuba', 'xylophone']
-['xylophone', 'tuba', 'trumpet', 'trombone', 'saxophone', 'guitar', 'french horn', 'flute', 'drum', 'clarinet']
-[2548, 5, 64891, 94, 0, 595158, 54, 2, 8535481567]
-[0, 2, 5, 54, 94, 2548, 64891, 595158, 8535481567]
-[8535481567, 595158, 64891, 2548, 94, 54, 5, 2, 0]
-clarinet
-xylophone
 saxophone
---
+++ Lists_Adv_Ranges_Slices.py
@@ -0,0 +1,32 @@
+tags = [
+    'python',
+    'development',
+    'tutorials',
+    'code',
+    'programming',
+    'computer science'
+]
+
+# tag_range = tags[1:-1]
+# print(tag_range)
+
+# ['development', 'tutorials', 'code', 'programming']
+
+# tag_range = tags[:-1:2] # third number is an interval(2=every other element in list)
+
+['python', 'tutorials', 'programming']
+
+# tag_range = tags[::-1]   # index value
+
+# print(tag_range)
+
+['computer science', 'programming', 'code', 'tutorials', 'development', 'python']
+
+# tags.sort(reverse=True) # alphabetically sorted reversed
+
+['tutorials', 'python', 'programming', 'development', 'computer science', 'code']
+
+# sorted_tags = tags.sort(reverse=True)    # Output None
+# cannot change the strings IMMUTABLE! Can sort but cannot reassign/store the values permanently
+
+# print(sorted_tags)
