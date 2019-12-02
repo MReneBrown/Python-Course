80fdbcf0cec5eee8f60df6ded69c082af341131f
M Rene Brown <rene_brown1@hotmail.com>
2019-12-02 11:20:07 -0600

    New_Key_Value_Pairs_Dictionary.py

---
+++ New_Key_Value_Pairs_Dictionary.py
@@ -0,0 +1,12 @@
+teams = {
+    "astros": ["Altuve", "Correa", "Bregman"],
+    "angels": ["Trout", "Pujols"],
+    "yankees": ["Judge", "Stanton"],
+}
+
+teams["red sox"] = ["Price", "Betts"]
+
+print(teams)
+
+{'astros': ['Altuve', 'Correa', 'Bregman'], 'angels': ['Trout', 'Pujols'], 
+'yankees': ['Judge', 'Stanton'], 'red sox': ['Price', 'Betts']}
