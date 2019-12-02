7362ad445c638fd9457c5e3af7bdb6d9c3583c0d
M Rene Brown <rene_brown1@hotmail.com>
2019-12-02 11:11:31 -0600

    Nested_Coll_Dictionaries.py

---
+++ Nested_Coll_Dictionaries.py
@@ -0,0 +1,15 @@
+teams = {
+    "astros": ["Altuve", "Correa", "Bregman"],
+    "angels": ["Trout", "Pujols"],
+    "yankees": ["Judge", "Stanton"],
+}
+
+astros = teams["astros"]
+
+print(astros)
+print(teams["astros"])
+print(teams["angels"])
+print(teams["yankees"])
+
+
+
