db6abdadd7de3c4446020c59ac4b22ed5533b0c8
M Rene Brown <rene_brown1@hotmail.com>
2019-12-10 16:38:59 -0600

    Tuple_Slices.py

---
+++ Tuple_Slices.py
@@ -0,0 +1,8 @@
+post = ('Python Basics', 'Intro Guide to Python', 'Some Cool Python Content', 'Published')
+
+# print(post [:2])
+# ('Python Basics', 'Intro Guide to Python')
+
+print(post[1::2])
+('Intro Guide to Python', 'Published')
+
