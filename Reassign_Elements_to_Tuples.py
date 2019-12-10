f781fa1a3a68100c7b49d58a1f85125f7360f5c4
M Rene Brown <rene_brown1@hotmail.com>
2019-12-10 13:48:50 -0600

    Reassign_Elements_to_Tuples.py

---
+++ Reassign_Elements_to_Tuples.py
@@ -0,0 +1,17 @@
+post = ('Python Basics', 'Intro Guide to Python', 'Some Cool Python Content')
+
+# post = post + ('Published',)
+
+print(id(post))
+print(id(post))
+
+post += ('Published',)
+
+print(id(post))
+
+# title, sub_heading, content, status = post
+
+# print(title)
+# print(sub_heading)
+# print(content)
+# print(status)
