126aceff0e9b9c96152db626ddf1580058a8a15a
M Rene Brown <rene_brown1@hotmail.com>
2019-12-10 14:48:27 -0600

    Tuples_Nested_Lists.py

---
+++ Tuples_Nested_Lists.py
@@ -0,0 +1,17 @@
+post = ('Python Basics', 'Intro Guide to Python', 'Some Cool Python Content')
+
+tags = ['python', 'coding', 'tutorial']
+
+post += (tags,)      
+
+# print(post)
+
+#('Python Basics', 'Intro Guide to Python', 'Some Cool Python Content', ['python', 'coding', 'tutorial'])
+
+# print(post[-1])
+
+# ['python', 'coding', 'tutorial']
+
+print(post[-1][1])
+
+coding
