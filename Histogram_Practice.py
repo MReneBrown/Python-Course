dfe6e9a419ae43f61d0701e1c5a4f309f7622fee
M Rene Brown <rene_brown1@hotmail.com>
2019-12-10 11:59:02 -0600

    Histogram_Practice.py

---
+++ Histogram_Practice.py
@@ -0,0 +1,19 @@
+"""
+wd $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+dd $$$$$$$$$$$$$$$$$$$$$$$
+ad $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+ud $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+"""
+
+sales = {
+    'web_design': 36,
+   'database_design': 23,
+    'api_design': 50,
+    'ux_design': 45,
+}
+
+print('wd  ' + sales['web_design'] * '$')
+print('dd  ' + sales['database_design'] * '$')
+print('ad  ' + sales['api_design'] * '$')
+print('ud  ' + sales['ux_design'] * '$')
+
