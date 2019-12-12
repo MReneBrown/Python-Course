ca17f229dfbb03a2a61e7a9d4eb61d214445a1d5
M Rene Brown <rene_brown1@hotmail.com>
2019-12-12 12:08:43 -0600

    Tuple_Dict_Key.py

---
+++ Tuple_Dict_Key.py
@@ -0,0 +1,17 @@
+priority_index = {
+    (1, 'premire'): [1, 34, 12],
+    (1, 'mvp'): [84, 22, 24],
+    (2, 'standard'): [93, 81, 3],
+}
+
+print(list(priority_index.keys()))
+
+print(list(priority_index.values()))
+
+print(list(priority_index.values())[0])
+
+[(1, 'premire'), (1, 'mvp'), (2, 'standard')]
+
+[[1, 34, 12], [84, 22, 24], [93, 81, 3]]
+
+[1, 34, 12]
