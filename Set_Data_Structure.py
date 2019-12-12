b36565da2c5dad2220c20967922d2d48c83c935c
M Rene Brown <rene_brown1@hotmail.com>
2019-12-12 14:20:18 -0600

    Set_Data_Structure.py

---
+++ Set_Data_Structure.py
@@ -0,0 +1,33 @@
+tags = {
+    'python',
+    'coding',
+    'tutorials',
+
+}
+
+# print(tags)
+
+# {'coding', 'tutorials', 'python'}
+
+
+# Nope
+# print(tags [0])
+
+# Traceback (most recent call last):
+#   File "Set_Data_Structure.py", line 12, in <module>
+#     print(tags [0])
+# TypeError: 'set' object is not subscriptable
+
+
+# query_one = 'python' in tags
+
+# print(query_one)
+
+# True
+
+
+query_two = 'ruby' in tags
+
+print(query_two)
+
+False
