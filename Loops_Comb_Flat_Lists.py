784f6b4c3a9040a8b3223206c2583ddd629ac097
M Rene Brown <rene_brown1@hotmail.com>
2019-12-15 11:55:18 -0600

    Loops_Comb_Flat_Lists.py

---
+++ Loops_Comb_Flat_Lists.py
@@ -0,0 +1,17 @@
+legacy_customers = ['Alice', 'Bob']
+new_customers = ['Tiffany', 'Kristine']
+
+raw_db = [legacy_customers, new_customers]
+
+print(raw_db)
+
+# [['Alice', 'Bob'], ['Tiffany', 'Kristine']]
+
+for legacy_customer in legacy_customers:
+    new_customers.append(legacy_customer)
+
+print(new_customers)
+
+# ['Tiffany', 'Kristine', 'Alice', 'Bob']
+
+
