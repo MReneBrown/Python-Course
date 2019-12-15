5c7943a64b2efad61e0f0861ca4f87c24269dcae
M Rene Brown <rene_brown1@hotmail.com>
2019-12-15 10:46:33 -0600

    Loop_Ranges.py

---
+++ Loop_Ranges.py
@@ -0,0 +1,45 @@
+# for num in range(1 , 10):
+#     print(num)
+
+# 1
+# 2
+# 3
+# 4
+# 5
+# 6
+# 7
+# 8
+# 9    
+
+# for num in range(1, 11):
+#     print(num)
+
+# 1
+# 2
+# 3
+# 4
+# 5
+# 6
+# 7
+# 8
+# 9
+# 10
+
+# for num in range(1, 11, 2):
+#     print(num)
+
+# 1
+# 3
+# 5
+# 7
+# 9
+
+for num in range(1, 11, 3):
+    print(num)
+
+1
+4
+7
+10    
+
+
