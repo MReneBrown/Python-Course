f493bdb9ef7b75418e0f9c039bd0202baa31ecf5
M Rene Brown <rene_brown1@hotmail.com>
2019-12-15 13:55:57 -0600

    Conditionals.py

---
+++ Conditionals.py
@@ -0,0 +1,40 @@
+# age = 25
+
+# if age < 25:
+#     print(f'I\'m sorry, you need to be at least 25 years old')
+
+# elif age > 100:
+#     print(f'I\'m sorry, {age} is too old to rent a car')
+
+# else:
+#     print(f'You\'re good to go, {age} fits in the range to rent a car')
+
+# You're good to go, 25 fits in the range to rent a car
+
+
+# age = 105
+
+# if age < 25:
+#     print(f'I\'m sorry, you need to be at least 25 years old')
+
+# elif age > 100:
+#     print(f'I\'m sorry, {age} is too old to rent a car')
+
+# else:
+#     print(f'You\'re good to go, {age} fits in the range to rent a car')
+
+# I'm sorry, 105 is too old to rent a car
+
+
+age = 45
+
+if age < 25:
+    print(f'I\'m sorry, you need to be at least 25 years old')
+
+elif age > 100:
+    print(f'I\'m sorry, {age} is too old to rent a car')
+
+else:
+    print(f'You\'re good to go, {age} fits in the range to rent a car')
+
+    # You're good to go, 45 fits in the range to rent a car
