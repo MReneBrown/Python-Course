7310699bbc00402b9cfe9659fb61cb6fd5f3798e
M Rene Brown <rene_brown1@hotmail.com>
2019-12-15 14:43:08 -0600

    Conditional_Operators_List.py

---
+++ Conditional_Operators_List.py
@@ -0,0 +1,53 @@
+# List of comparison operators
+# == Equality
+# != Inequality
+# <> Inequality (deprecated)  NO LONGER BEING WORKED ON. SHOULD NOT USE THIS OPERATOR!
+# >  Greater than
+# >= Greater than or equal to
+# < Less than
+# <= Less than or equal to
+
+# # username = 'jonsnow'
+
+# if username == 'jonsnow':
+#   print('Welcome!')
+# else:
+#   print('You shall not pass!')
+
+# Welcome!
+
+# username = 'jonsnow'
+
+# if username != 'jonsnow':
+#   print('Welcome!')
+# else:
+#   print('You shall not pass!')
+
+# You shall not pass!
+
+
+# age = 55
+
+# if age > 10:
+#   print('Welcome!')
+# else:
+#   print('You shall not pass!')
+
+# Welcome!
+
+user_list = ['Kristine', 'Tiffany']
+user_list2 = ['Jordan', 'Braden']
+
+if user_list == user_list2:
+    print('They match!')
+
+# prints nothing because the lists do not match
+
+user_list = ['Kristine', 'Tiffany']
+user_list2 = ['Kristine', 'Tiffany']
+
+if user_list == user_list2:
+    print('They match!')
+
+They match!
+
