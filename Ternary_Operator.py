661247f765943248759ea4cceaae2c35ff4df3ca
M Rene Brown <rene_brown1@hotmail.com>
2019-12-15 14:19:13 -0600

    Ternary_Operator.py

---
+++ Ternary_Operator.py
@@ -0,0 +1,16 @@
+role = 'admin'
+
+auth = 'can access' if role == 'admin' else 'cannot access'
+
+"""
+traditional syntax of conditional
+if role == 'admin'
+    auth = 'can access'
+else:
+    auth = 'cannot access'
+"""
+
+print(auth)
+
+cann access
+
