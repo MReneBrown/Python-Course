3c6ab13ae9d776e0172a2e6c676361fd1c3dc5f7
M Rene Brown <rene_brown1@hotmail.com>
2019-12-16 15:11:14 -0600

    Cond_Comp_Prac.py

---
+++ Cond_Comp_Prac.py
@@ -0,0 +1,37 @@
+# user = 'markhenry'
+# email = 'mhenry43@yahoo.com'
+# password = 'shemademedoit'
+
+# if user == 'markhenry' and password == 'hemademedoit':
+#     print('You may not have access')
+# else:
+#     print('Congratulations! You now have access!')
+
+# Congratulations! You now have access!
+
+# ********************************************************
+
+# user = 'markhenry'
+# email = 'mhenry43@yahoo.com'
+# password = 'shemademedoit'
+
+# if (user == 'markhenry' or email == 'mhenry43@yahoo.com') and password == 'shemademedoit':
+#     print('Congratulations! You now have access!')
+# else:
+#     print('You may not have access')
+
+# Congratulations! You now have access!
+
+# ********************************************************
+
+user = 'markhenry'
+email = 'mhenry43@yahoo.org'
+password = 'shemademedoit'
+
+if (user == 'markhenry' and email == 'mhenry43@yahoo.com') and password == 'shemademedoit':
+    print('Congratulations! You now have access!')
+else:
+    print('You may not have access')
+
+You may not have access
+
