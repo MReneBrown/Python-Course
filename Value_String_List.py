4a7785dd7310aea24ee8f81e15852e7ac9696c0c
M Rene Brown <rene_brown1@hotmail.com>
2019-12-16 14:27:58 -0600

    Value_String_List.py

---
+++ Value_String_List.py
@@ -0,0 +1,50 @@
+# sentence = 'The quick brown fox jumped over the lazy Dog'
+# word = 'dog'
+
+# if word in sentence:
+#     print('The word was found in the sentence')
+# else:
+#     print('The word was not found in the sentence')
+
+# The word was found in the sentence
+
+# *********************************************************
+
+# if word in sentence:
+#     print('The word was found in the sentence')
+# else:
+#     print('The word was not found in the sentence')
+
+# The word was not found in the sentence   
+#  Word has to match case with word in the sentence 'dog' != 'Dog
+
+# *********************************************************
+
+
+# if word.lower() in sentence.lower():
+#     print('The word was found in the sentence')
+# else:
+#     print('The was not found in the sentence')
+
+# The word was found in the sentence
+
+# *********************************************************
+
+nums = [1, 2, 3, 4]
+
+# if 3 in nums:
+#     print('The number was found')
+# else:
+#     print('The number was not found')
+
+# The number was found
+
+# *********************************************************
+
+
+if 10 in nums:
+    print('The number was found')
+else:
+    print('The number was not found')
+
+The number was not found
