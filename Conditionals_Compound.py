31aaaa5f84638e03924c266012a36e5ef87845e8
M Rene Brown <rene_brown1@hotmail.com>
2019-12-16 15:01:50 -0600

    Conditionals_Compound.py

---
+++ Conditionals_Compound.py
@@ -0,0 +1,123 @@
+# username = 'jonsnow'
+# email = 'jon@snow.com'
+# password = 'thenorth'
+
+# if username == 'jonsnow' and password == 'thenorth':
+#     print('Access permitted')
+# else:
+#     print('You shall not pass!')
+
+# Access permitted
+
+# ******************************************************
+
+# username = 'sansa'
+# email = 'jon@snow.com'
+# password = 'thenorth'
+
+# if username == 'jonsnow' and password == 'thenorth':
+#     print('Access permitted')
+# else:
+#     print('You shall not pass!')
+
+# You shall not pass!
+
+# ******************************************************
+
+# username = 'sansa'
+# email = 'jon@snow.com'
+# password = 'thenorth'
+
+# if username == 'jonsnow' or password == 'thenorth':
+#     print('Access permitted')
+# else:
+#     print('You shall not pass!')
+
+# Access permitted
+
+# ******************************************************
+
+# username = 'jonsnow'
+# email = 'jon@snow.com'
+# password = 'thenorth'
+
+# if (username == 'jonsnow' or email == 'jon@snow.com') and password == 'thenorth':
+#     print('Access permitted')
+# else:
+#     print('You shall not pass!')
+
+# Access permitted
+
+# ******************************************************
+
+
+
+# username = ''
+# email = 'jon@snow.com'
+# password = 'thenorth'
+
+# if (username == 'jonsnow' or email == 'jon@snow.com') and password == 'thenorth':
+#     print('Access permitted')
+# else:
+#     print('You shall not pass!')
+
+# Access permitted
+
+# ******************************************************
+
+# username = ''
+# email = 'jon@snow.net'
+# password = 'thenorth'
+
+# if (username == 'jonsnow' or email == 'jon@snow.com') and password == 'thenorth':
+#     print('Access permitted')
+# else:
+#     print('You shall not pass!')
+
+# You shall not pass!
+
+# ******************************************************
+
+# username = ''
+# email = 'jon@snow.com'
+# password = 'asdfasdf'
+
+# if (username == 'jonsnow' or email == 'jon@snow.com') and password == 'thenorth':
+#     print('Access permitted')
+# else:
+#     print('You shall not pass!')
+
+# You shall not pass!
+
+# ******************************************************
+
+# logged_in = True
+# standard_user = True
+
+# if logged_in and not standard_user:
+#     print('You can access the Admin Dashboard')
+# else:
+#     print('You can only access the Standard Dashboard')
+
+# You can only access the Standard Dashboard
+
+# ******************************************************
+
+logged_in = True
+standard_user = False
+
+if logged_in and not standard_user:
+    print('You can access the Admin Dashboard')
+else:
+    print('You can only access the Standard Dashboard')
+
+You can access the Admin Dashboard
+
+
+
+
+
+
+
+
+
