8364fbe7b8b877982e550854978a4a48352cbd3c
M Rene Brown <rene_brown1@hotmail.com>
2019-12-18 13:01:45 -0600

    Function_Syntax.py

---
+++ Function_Syntax.py
@@ -0,0 +1,261 @@
+# def full_name():
+#     print('Hi')
+
+
+# full_name()
+
+# Hi
+
+# def full_name(first, last):
+#     print(f'{first} {last}')
+    
+
+# full_name('Kristine', 'Hudgens')
+# full_name('Tiffany', 'Hudgens')
+
+# Kristine Hudgens
+# Tiffany Hudgens
+
+# ----------------------------------
+
+# def auth(email, password):
+#     if email == 'kristine@hudgens.com' and password == 'secret':
+#         print('Authorized')
+#     else:
+#         print('Not Authorized')
+
+    
+# auth('jordan@hudgens.com', 'secret')
+
+# Not Authorized
+
+# ----------------------------------
+
+# def auth(email, password):
+#     if email == 'kristine@hudgens.com' and password == 'secret':
+#         print('Authorized')
+#     else:
+#         print('Not Authorized')
+
+    
+# auth('kristine@hudgens.com', 'secret')
+
+# Authorized
+
+# ----------------------------------
+
+# def hundred():
+#     for num in range(1, 101):
+#         print(num)
+
+
+# hundred()
+
+# 1
+# 2
+# 3
+# 4
+# 5
+# 6
+# 7
+# 8
+# 9
+# 10
+# 11
+# 12
+# 13
+# 14
+# 15
+# 16
+# 17
+# 18
+# 19
+# 20
+# 21
+# 22
+# 23
+# 24
+# 25
+# 26
+# 27
+# 28
+# 29
+# 30
+# 31
+# 32
+# 33
+# 34
+# 35
+# 36
+# 37
+# 38
+# 39
+# 40
+# 41
+# 42
+# 43
+# 44
+# 45
+# 46
+# 47
+# 48
+# 49
+# 50
+# 51
+# 52
+# 53
+# 54
+# 55
+# 56
+# 57
+# 58
+# 59
+# 60
+# 61
+# 62
+# 63
+# 64
+# 65
+# 66
+# 67
+# 68
+# 69
+# 70
+# 71
+# 72
+# 73
+# 74
+# 75
+# 76
+# 77
+# 78
+# 79
+# 80
+# 81
+# 82
+# 83
+# 84
+# 85
+# 86
+# 87
+# 88
+# 89
+# 90
+# 91
+# 92
+# 93
+# 94
+# 95
+# 96
+# 97
+# 98
+# 99
+# 100
+
+
+def counter(max_value):
+    for num in range(1, max_value):
+        print(num)
+
+counter(101)
+
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+20
+21
+22
+23
+24
+25
+26
+27
+28
+29
+30
+31
+32
+33
+34
+35
+36
+37
+38
+39
+40
+41
+42
+43
+44
+45
+46
+47
+48
+49
+50
+51
+52
+53
+54
+55
+56
+57
+58
+59
+60
+61
+62
+63
+64
+65
+66
+67
+68
+69
+70
+71
+72
+73
+74
+75
+76
+77
+78
+79
+80
+81
+82
+83
+84
+85
+86
+87
+88
+89
+90
+91
+92
+93
+94
+95
+96
+97
+98
+99
+100
