2fed66dfac06faebf6cfa067b0f977b4d3433eab
M Rene Brown <rene_brown1@hotmail.com>
2019-12-19 10:38:59 -0600

    Function_Nesting.py

---
+++ Function_Nesting.py
@@ -0,0 +1,21 @@
+# def greeting(first, last):
+#     def full_name():
+#         return f'{first} {last}'
+
+
+#     print(f'Hi {full_name()}!')
+
+# greeting('Kristine', 'Hudgens')
+
+
+
+
+def birth_month_year(mo_yr):
+    def report():
+        return f'{month} {year}'
+
+    print(f'Your birth month and year are {mo_yr}')
+
+report('September' , '1969')
+
+
