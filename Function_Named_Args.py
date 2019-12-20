29468398f707d46cb1844b356325fdd0ec305321
M Rene Brown <rene_brown1@hotmail.com>
2019-12-20 12:21:37 -0600

    Function_Named_Args.py

---
+++ Function_Named_Args.py
@@ -0,0 +1,46 @@
+# def full_name(first, last):
+#     print(f'{first} {last}')
+
+
+# full_name('Kristine', 'Hudgens')
+
+# Kristine Hudgens
+
+# def full_name(first, last):
+#     print(f'{first} {last}')
+
+
+# full_name(first = 'Kristine', last = 'Hudgens')
+
+# Kristine Hudgens
+
+
+# def full_name(first, last):
+#     print(f'{first} {last}')
+
+
+# full_name(last = 'Hudgens', first = 'Kristine')
+
+# Kristine Hudgens
+
+
+# def full_name(first, mi, last):
+#     print(f'{first} {mi} {last}')
+
+
+# full_name(first = 'Rylee', mi = 'Callahan', last = 'Brown')
+# full_name(first = 'Liam',  mi = 'Anekin', last = 'Brown')
+# full_name(first = 'Nathaniel', mi = 'Drew', last = 'Brown')
+# full_name(first = 'Kayla',  mi = 'Elaine', last = 'Brown')
+
+# Rylee Callahan Brown
+# Liam Anekin Brown
+# Nathaniel Drew Brown
+# Kayla Elaine Brown
+
+def my_function(*kids):
+    print("My youngest child is " + kids[3])
+
+
+my_function('Nathaniel', 'Justin', 'David', 'Rebekah')
+
