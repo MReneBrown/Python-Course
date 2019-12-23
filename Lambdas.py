59e5885816b5c405d06c6cded32c27035ce26593
M Rene Brown <rene_brown1@hotmail.com>
2019-12-23 13:55:03 -0600

    Lambdas.py

---
+++ Lambdas.py
@@ -0,0 +1,11 @@
+full_name = lambda first, last: f'{first} {last}'
+
+def greeting(name):
+    print(f'Hi there {name}!')
+
+
+greeting(full_name('Tiffany', 'Hudgens'))
+
+
+Hi there Tiffany Hudgens!
+
