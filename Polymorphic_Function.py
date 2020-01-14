090add9b301b151bfe07ecde09b4fd1043534b1e
M Rene Brown <rene_brown1@hotmail.com>
2020-01-14 12:35:25 -0600

    Polymorphic_Function.py

---
+++ Polymorphic_Function.py
@@ -0,0 +1,29 @@
+class Heading:
+    def __init__(self, content):
+        self.content = content
+
+    def render(self):
+        return f'<h1>{self.content}</h1>'
+
+class Div:
+    def __init__(self, content):
+        self.content = content
+
+    def render(self):
+        return f'<div>{self.content}</div>'
+
+div_one = Div('Some Content')
+heading = Heading('Some Big Heading')
+div_two = Div('Another Div')
+
+def html_render(tag_object):
+    print(tag_object.render())
+
+html_render(div_one)
+html_render(heading)
+html_render(div_two)
+
+
+<div>Some Content</div>
+<h1>Some Big Heading</h1>
+<div>Another Div</div>
