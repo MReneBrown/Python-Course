27c0735b04aa2ac194bf483566de2f5a100116bf
M Rene Brown <rene_brown1@hotmail.com>
2020-01-15 13:09:09 -0600

    Pretty_Price.py

---
+++ Pretty_Price.py
@@ -0,0 +1,34 @@
+# def pretty_price(gross_price, extension):
+#     return int(gross_price) + extension
+
+# print(pretty_price(3.50, .95))
+
+# 3.95
+
+
+def pretty_price(gross_price, extension):
+    if isinstance(extension, int):
+        extension = extension * 0.01
+    return int(gross_price) + extension
+
+print(pretty_price(3.50, .95))
+
+print(pretty_price(3.50, 95))
+
+print(pretty_price(3.20, 99))
+print(pretty_price(3.99, 0.20))
+print(pretty_price(3.20, .95))
+print(pretty_price(3, 95))
+print(pretty_price(3, 2))
+
+
+
+3.95
+3.95
+3.99
+3.2
+3.95
+3.95
+3.02
+
+
