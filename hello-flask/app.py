67aa794c144e7b6a438b9d7578cf539b60c95496
M Rene Brown <rene_brown1@hotmail.com>
2020-01-16 10:34:09 -0600

    app.py

--- hello-flask/app.py
+++ hello-flask/app.py
@@ -1,14 +1,18 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+import os
 
 app = Flask(__name__)
 
 @app.route('/')
-
 def hello():
     return "Hey Flask!"
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
 
 
 # E:\school\FULL-STACK-COURSE\Python-Course\hello-flask>pipenv install flask
