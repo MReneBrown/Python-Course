2c357d5adebb77756772a077f9c548f210459885
M Rene Brown <rene_brown1@hotmail.com>
2020-01-22 19:00:07 -0600

    app.py, app.sqlite

--- hello-flask/app.py
+++ hello-flask/app.py
@@ -19,15 +19,16 @@ class Guide(db.Model):
         self.title = title
         self.content = content
 
+
 class GuideSchema(ma.Schema):
     class Meta:
         fields = ('title', 'content')
+
 
 guide_schema = GuideSchema()
 guides_schema = GuideSchema(many=True)
 
 # Endpoint to create a new guide
-
 @app.route('/guide', methods=["POST"])
 def add_guide():
     title = request.json['title']
@@ -39,11 +40,49 @@ def add_guide():
     db.session.commit()
 
     guide = Guide.query.get(new_guide.id)
-    
+
     return guide_schema.jsonify(guide)
 
+# Endpoint to query all guides
+
+@app.route("/guides", methods=["GET"])
+def get_guides():
+    all_guides = Guide.query.all()
+    result = guides_schema.dump(all_guides)
+    return jsonify(result)
 
 
+#  Endpoint for querying a single guide
+
+@app.route("/guide/<id>", methods=["GET"])
+def get_guide(id):
+    guide = Guide.query.get(id)
+    return guide_schema.jsonify(guide)
+    
+# Endpoint for updating a guide
+@app.route("/guide/<id>", methods=["PUT"])
+def guide_update(id):
+    guide = Guide.query.get(id)
+    title = request.json['title']
+    content = request.json['content']
+
+    guide.title = title
+    guide.content = content
+
+    db.session.commit()
+    return guide_schema.jsonify(guide)
+
+# Endpoint for deleting a record
+
+@app.route("/guide/<id>", methods=["DELETE"])
+def guide_delete(id):
+    guide = Guide.query.get(id)
+    db.session.delete(guide)
+    db.session.commit()
+
+    # return guide_schema.jsonify(guide)
+    return "Guide was successfully deleted"
+    
 if __name__ == '__main__':
     app.run(debug=True)
 
